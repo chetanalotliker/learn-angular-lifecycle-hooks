9da117e62bf1fd325416b900df5dc9c456d8854d
chetana.lotliker <chetanalotliker@gmail.com>
2024-02-10 22:11:27 +0530

    Create prod ready version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { ChildComponent } from './components/child/child.component';
+import { ChildComponent } from './components/child/child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    // ChildComponent
+    ChildComponent
   ],
   imports: [
     BrowserModule,
